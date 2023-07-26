1e085ec9944c038ed64a8f8abf9f51e48ee2c5e5
Morgan Strohl <morgan.strohl@gmail.com>
2023-07-26 10:39:45 +0200

    commit -m "WEBPACK - Modification config webpack"

--- webpack.config.js
+++ webpack.config.js
@@ -2,32 +2,37 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: path.resolve(__dirname, "src/index.js"),
+  entry: {
+    main: path.join(__dirname, "src/index.js"),
+  },
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.join(__dirname, "dist"),
     filename: "[name].bundle.js",
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
+        test: /\.js/,
+        exclude: /(node_modules)/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "src/index.html"),
+      template: path.join(__dirname, "src/index.html"),
     }),
   ],
+  stats: "minimal",
   devtool: "source-map",
   mode: "development",
   devServer: {
+    open: false,
     static: path.resolve(__dirname, "./dist"),
-    open: true,
     watchFiles: ["./src/**"],
     port: 4000,
     hot: true,
