a08cce6690419e0cf6f318aa40ffa46f346efc99
Morgan Strohl <morgan.strohl@gmail.com>
2023-07-26 11:42:21 +0200

    commit -m "DOM TO DO - Récupération de la référence DOM et création du DOM pour chaque to do existante"

--- src/Index.js
+++ src/Index.js
@@ -1,1 +1,34 @@
 import "./style.css";
+
+const ul = document.querySelector("ul");
+
+// const todos = [
+//   {
+//     text: "1ere chose à faire",
+//     done: true,
+//   },
+//   {
+//     text: "2nde chose à faire",
+//     done: false,
+//   },
+// ];
+
+function todoCreateElements(todo, index) {
+  const li = document.createElement("li");
+  li.innerHTML = `
+    <span class="todo ${todo.done ? " done" : ""}  "></span>
+    <p>${todo.text}</p>
+    <button>Supprimer</button>
+    `;
+  return li;
+}
+
+function todo() {
+  const todoMap = todos.map((todo, index) => {
+    return todoCreateElements(todo, index);
+  });
+  ul.innerHTML = "";
+  ul.append(...todoMap);
+}
+
+todo();
